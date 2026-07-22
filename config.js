5712c49ef27af74b526e00818a4e758189cf8008
spararouma <Marwan7bibi007@gmail.com>
2026-07-22 23:42:13 +0100

    Update pairing number and owner phone number

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ global.ButtonV2 = ButtonV2;
 global.Carousel = Carousel;
 global.AIRich = AIRich;
 
-global.pairingNumber = 212666666666;
+global.pairingNumber = 212615154160;
 global.owner = [
-  ['212717457920', 'noureddine Ouafy', true],
+  ['2212615154160', 'noureddine Ouafy', true],
   ['', 'Owner 2', true],
 ];
 
